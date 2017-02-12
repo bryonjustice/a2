e18bf74f063a1d55c79b9004129e30739aa12c1b
Bryon Justice <bryon_justice@yahoo.com>
2017-02-12 17:28:41 -0500

    Check-in baseline site materials

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>DWA 15 :: Assignment 2 - PHP</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootswatchcdn.com/bootstrap/3.3.7/flatly/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/a2.css" />
+    </head>
+    <body>
+        <h1>Assignment 2 - PHP</h1>
+
+    </body>
+</html>
